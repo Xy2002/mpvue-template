0d3b9a6778c1f4885dc17f9e1caef44aad9f6592
Marshall <57112423+Xy2002@users.noreply.github.com>
2021-11-01 10:57:28 +0800

    update

--- template/src/main.js
+++ template/src/main.js
@@ -4,9 +4,11 @@
 {{/if_eq}}
 import Vue from 'vue'
 import App from './App'
-
+import store from './store/index'
+import request from './utils/request'
 Vue.config.productionTip = false
 App.mpType = 'app'
-
+Vue.prototype.$store = store
+Vue.prototype.$http = request
 const app = new Vue(App)
 app.$mount()
---
+++ template/src/utils/request.js
@@ -0,0 +1,45 @@
+import Fly from 'flyio/dist/npm/wx'
+
+const request = new Fly()
+const showToast = (title) => {
+  wx.showToast({
+    title: title + '',
+    icon: 'none'
+  })
+}
+
+request.config.timeout = 3 * 1000
+request.config.baseURL = 'http://localhost:20000/apis/'
+request.config.headers = {'content-type': 'application/x-www-form-urlencoded'}
+request.interceptors.request.use((request) => {
+  wx.showLoading({ title: '加载中...' })
+  return request
+})
+
+
+request.interceptors.response.use(
+  (response) => {
+    wx.hideLoading()
+    return response.data
+  },
+  (err) => {
+    wx.hideLoading()
+    console.log(err)
+    let errCode = err.status
+    switch (errCode) {
+      case 1:
+        showToast('Timeout')
+        break
+      case 404:
+        showToast('404 Not Found')
+        break
+      default:
+        showToast('Unknown error')
+        break
+    }
+
+    return err.message
+  }
+)
+
+export default request
